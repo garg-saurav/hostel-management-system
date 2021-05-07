c0b8e1321701baa3b561805fa18b71f43f83f58d
Sahil Jain <jsahil730@gmail,com>
2021-05-08 02:26:44 +0530

    minor fix 2

--- controllers/add_hostel.js
+++ controllers/add_hostel.js
@@ -29,7 +29,7 @@ exports.post_add_hostel = async (req, res, next) => {
     const addr = req.body.address;
     const additional = req.body.additional;
     const services = JSON.parse(req.body.servicenames);
-    const images = req.body.hostelimages;
+    const images = req.files.map(e => e.path);
     const decoded = verify.authenticate(req);
 
     if (decoded) {
